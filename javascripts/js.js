a1a8f14669fd2e8f742939ca3b48452ff797a2a1
polirybka <barybinapoli@gmail.com>
2022-03-14 16:15:17 +0300

    fknv

--- javascripts/js.js
+++ javascripts/js.js
@@ -1,4 +1,31 @@
+// Вылетает менюшка
+$(document).ready(function() {
+$('a.button').click( function(event){
+event.preventDefault();
+$('#myOverlay').fadeIn(297,function(){
+      $('#myModal')
+      .css('display', 'block')
+      .animate({opacity: 1}, 198);
+});
+});
 
+$('#myModal__close').click( function(){
+$('#myModal').animate({opacity: 0}, 198,
+      function(){
+        $(this).css('display', 'none');
+        $('#myOverlay').fadeOut(297);
+});
+});
+});
+// кнопки в менюшке
 $(document).ready(function(){
-$("#demo").html("hi")
+  $("form#show").click(function(){
+    event.preventDefault();
+
+    $("#img1").toggle();
+    if($("#img1").is(':visible'))
+      $("#img2").hide();
+    else
+      $("#img2").show();
+  });
 });
